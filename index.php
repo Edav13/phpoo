d7303c96a9357ece2e49695dbe33214eef635a80
Edav13 <Edav13@hotmail.com>
2016-06-19 16:03:46 -0400

    Modificadores de acceso

--- index.php
+++ index.php
@@ -1,39 +1,29 @@
 <?php  
     
-    class Loteria{
-       public $numero;
-       public $intentos;
-       public $resultado = false;
 
-       public function __construct($numero, $intentos){
-            $this->numero = $numero;
-            $this->intentos = $intentos;
-       }
-       public function sortear(){
-            $minimo = $this->numero / 2;
-            $maximo = $this->numero * 2;
-            
-            for ($i=0; $i < $this->intentos; $i++) { 
-                $int = rand($minimo, $maximo);
-                self::intentos($int);
-            }
-       }
-       public function intentos($int){
-            if ($int == $this->numero) {
-                echo "<b>".$int." == ".$this->numero."</b><br>";
-                $this->resultado = true;
-            }else{
-                echo "<b>".$int." != ".$this->numero."</b><br>";
-            }
-       }
-       public function __destruct(){
-            if($this->resultado){
-                echo "Felicidades, has acertado en ". $this->intentos. " intentos.";
-            }else{
-                echo "Que lastima, has perdido en ". $this->intentos. " intentos.";
-            }
-       }
+    class Facebook
+    {
+        public $nombre;
+        public $edad;
+        private $pass;
+
+        function __construct($nombre, $edad, $pass)
+        {
+            $this->nombre = $nombre;
+            $this->edad = $edad;
+            $this->pass = $pass;
+        }
+        public function verInformacion(){
+            echo "Nombre: ".$this->nombre."<br>";
+            echo "Edav: ".$this->edad."<br>";
+            self::cambiarPass("54321");
+            echo "Password: ".$this->pass;
+        }
+        private function cambiarPass($pass){
+            $this->pass = $pass;
+        }
     }
-    $loteria = new Loteria(10, 10);
-    $loteria->sortear();
+    $facebook = new Facebook("Carlos Fernandez", 20, "1234");
+    $facebook->verInformacion();
+
 ?>
