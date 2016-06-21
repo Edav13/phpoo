224baf1998984a283319e47540400aa0faed2c85
Edav13 <Edav13@hotmail.com>
2016-06-21 13:50:05 -0400

    Clases y Metodos Abstractos

--- index.php
+++ index.php
@@ -1,69 +1,22 @@
 <?php 
-    interface Auto{
-        public function encender();
-        public function apagar();
+    abstract class Molde{
+        abstract public function ingresarNombre($nombre);
+        abstract public function obtenerNombre();
     }
-    interface gasolina extends Auto{
-        public function vaciarTaque();
-        public function llenarTanque($cant);
-    }
-    class Deportivo implements gasolina{
-        private $estado = false;
-        private $tanque = 0;
-        public function estado(){
-            if($this->estado){
-                echo "El auto esta encendido y tiene ".$this->tanque." de litros en el tanque<br>";
-            }
-            else{
-                echo "El auto esta apagado <br>";
-            }
-                
+    class Persona extends Molde{
+        private $mensaje = "hola gente de codigoFacilito";
+        private $nombre;
+        public function mostrar(){
+            echo $this->mensaje;
         }
-         public function encender(){
-            if($this->estado){
-                echo "No puedes encender el auto 2 veces <br>";
-            }
-            else{
-                if ($this->tanque <= 0) {
-                    echo "Usted no puede encender el auto porque el tanque esta vacio <br>";
-                }else{
-                    echo "Auto encendido <br>";
-                    $this->estado = true;
-                }
-                
-            }
-         }
-        public function apagar(){
-            if($this->estado){
-                echo "Auto apagado <br>";
-                $this->estado = false;
-            }
-            else{
-                echo "Auto esta apagado <br>";
-                //$this->estado = true;
-            }
-        }       
-        public function vaciarTaque(){
-            $this->tanque = 0;
+        public function ingresarNombre($nombre, $username = " CF"){
+            $this->nombre = $nombre . $username;
         }
-        public function llenarTanque($cant){
-            $this->tanque = $cant;
-        }
-        public function usar($km){
-            if ($this->estado) {
-                $reducir = $km / 3;
-                $this->tanque = $this->tanque - $reducir;
-                if ($this->tanque <= 0) 
-                    $this->estado = false;
-                
-            }else{
-                echo "El auto esta apagado y no se puede usar <br>";
-            }
+        public function obtenerNombre(){
+            echo $this->nombre;
         }
     }
-    $obj = new Deportivo();
-    $obj->llenarTanque(100);
-    $obj->encender();
-    $obj->usar(390);
-    $obj->estado();
+    $obj = new Persona();
+    $obj-> ingresarNombre("Carlos", " Fernadez");
+    $obj->obtenerNombre();
 ?>
