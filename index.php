9114739033bb2f30a6b36400b168f4a4a1f1a22a
Edav13 <Edav13@hotmail.com>
2016-06-21 13:18:55 -0400

    Interfaces en php

--- index.php
+++ index.php
@@ -1,19 +1,69 @@
-<?php  
-    class Pagina{
-        public $nombre = "Codigo Facilito";
-        public static $url = "www.codigofacilito.com";
-
-        public function bienvenida(){
-            echo "Bienvendios a <b>".$this->nombre."</b> la pagina es <b>".self::$url."</b>";
+<?php 
+    interface Auto{
+        public function encender();
+        public function apagar();
+    }
+    interface gasolina extends Auto{
+        public function vaciarTaque();
+        public function llenarTanque($cant);
+    }
+    class Deportivo implements gasolina{
+        private $estado = false;
+        private $tanque = 0;
+        public function estado(){
+            if($this->estado){
+                echo "El auto esta encendido y tiene ".$this->tanque." de litros en el tanque<br>";
+            }
+            else{
+                echo "El auto esta apagado <br>";
+            }
+                
         }
-        public function bienvenida2(){
-            echo "<br>Bienvenidos ".self::$url;
+         public function encender(){
+            if($this->estado){
+                echo "No puedes encender el auto 2 veces <br>";
+            }
+            else{
+                if ($this->tanque <= 0) {
+                    echo "Usted no puede encender el auto porque el tanque esta vacio <br>";
+                }else{
+                    echo "Auto encendido <br>";
+                    $this->estado = true;
+                }
+                
+            }
+         }
+        public function apagar(){
+            if($this->estado){
+                echo "Auto apagado <br>";
+                $this->estado = false;
+            }
+            else{
+                echo "Auto esta apagado <br>";
+                //$this->estado = true;
+            }
+        }       
+        public function vaciarTaque(){
+            $this->tanque = 0;
+        }
+        public function llenarTanque($cant){
+            $this->tanque = $cant;
+        }
+        public function usar($km){
+            if ($this->estado) {
+                $reducir = $km / 3;
+                $this->tanque = $this->tanque - $reducir;
+                if ($this->tanque <= 0) 
+                    $this->estado = false;
+                
+            }else{
+                echo "El auto esta apagado y no se puede usar <br>";
+            }
         }
     }
-    class Web extends Pagina{
-
-    }
-    $pagina = new Pagina();
-    $pagina->bienvenida();
-    Web::bienvenida2();
+    $obj = new Deportivo();
+    $obj->llenarTanque(100);
+    $obj->encender();
+    $obj->usar(390);
+    $obj->estado();
 ?>
