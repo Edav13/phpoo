622ea749ac96d600982ca039c91e6d7a87356af8
Edav13 <Edav13@hotmail.com>
2016-06-22 22:09:59 -0400

    Manejo de traits en php

--- index.php
+++ index.php
@@ -1,8 +1,27 @@
 <?php 
-    function autoload($clase){
-        include "clases/".$clase.".php";
+    trait Persona1{
+        public $nombre;
+        public function mostrarNombre(){
+            echo $this->nombre;
+        }
+        abstract function asignarNombre($nombre);
     }
-    spl_autoload_register("autoload");
-    Persona::mostrar("Hola mundo<br>");
-    Auto::mostrar("Codigo Facilito");
+    trait Trabajador{
+        protected function mensaje(){
+            echo " y Soy un trabajador.";
+        }
+    }
+
+    class Persona{
+        use Persona1, Trabajador;
+
+        public function asignarNombre($nombre){
+            $this->nombre = $nombre;
+            $this->mostrarNombre();
+            self::mensaje();
+        }
+    }
+    $persona = new Persona();
+    $persona->asignarNombre("Carlos");
+    
 ?>
